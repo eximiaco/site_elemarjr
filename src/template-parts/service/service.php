90cd863def3a61d809508e9937d9b49e90665af5
Nicolas Huber <nicolasluishuber@gmail.com>
2019-01-17 10:09:04 -0200

    Fix card height on mobile devices

--- src/template-parts/service/service.php
+++ src/template-parts/service/service.php
@@ -22,7 +22,7 @@ global $container;
 			<div class="card__thumbnail"><?php the_post_thumbnail(); ?></div>
 		</div>
 		<div class="card__content">
-			<?php echo esc_html( mb_strimwidth( get_the_content(), 0, 320, '...' ) ); ?>
+			<?php echo wp_kses_post( mb_strimwidth( get_the_content(), 0, 320, '...' ) ); ?>
 		</div>
 		<div class="card__footer">
 			<a href="<?php the_permalink(); ?>" class="card__button">Saiba mais</a>
