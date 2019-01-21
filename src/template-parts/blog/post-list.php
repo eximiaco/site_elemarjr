f8d4210d784757615667970199c741febc5abe75
Nicolas Huber <nicolasluishuber@gmail.com>
2019-01-21 13:53:33 -0200

    Add card list wrapper on homepage

--- src/template-parts/blog/post-list.php
+++ src/template-parts/blog/post-list.php
@@ -16,12 +16,14 @@ $extra_class = $container->get( 'post_list.extra_class' );
 <div class="post-list--wrapper">
 	<div class="post-list--loading"><?php esc_html_e( 'Loading posts...', 'elemarjr' ); ?></div>
 	<div class="post-list cards-list <?php echo esc_attr( $extra_class ); ?>">
-		<?php
+		<div class="cards-list__wrapper">
+        <?php
 		while ( $query->have_posts() ) :
 			$query->the_post();
 			get_template_part( 'template-parts/blog/content' );
 			endwhile;
 			wp_reset_postdata();
 		?>
+        </div>
 	</div>
 </div>
