daaaafd3a51226f07317544eda5fc10dfb8921d9
Nicolas Huber <nicolasluishuber@gmail.com>
2019-01-24 16:11:19 -0200

    Copy images and fonts on grunt dist

--- app/tasks/options/copy.js
+++ app/tasks/options/copy.js
@@ -2,6 +2,18 @@
  * Clean files and folders
  */
 module.exports = {
+	images: {
+		expand: true,
+		cwd: '<%= config.assets.src %>/images',
+		src: '**/*',
+		dest : '<%= config.assets.build %>/images'
+	},
+	'font-icon': {
+		expand: true,
+		cwd: '<%= config.assets.src %>/font-icon/fonts',
+		src: '**/*',
+		dest : '<%= config.assets.build %>/fonts/icons/'
+	},
 	assets: {
 		expand: true,
 		cwd: '<%= config.assets.build %>',
