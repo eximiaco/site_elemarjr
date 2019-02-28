bcb4741ce243156863d8f53f0c3e06b721cd2709
Eduardo Pittol <edpittol@gmail.com>
2019-02-27 21:29:36 -0300

    Ajustado compilação arquivo login.styl

--- app/tasks/options/stylus.js
+++ app/tasks/options/stylus.js
@@ -9,7 +9,7 @@ module.exports = {
 			'include css': true
 		},
 		files : {
-			'<%= config.assets.build %>/css/login.css' : '<%= config.assets.src %>/stylus/pages/login.styl',
+			'<%= config.assets.build %>/css/login.css' : '<%= config.assets.src %>/stylus/login.styl',
 			'<%= config.assets.build %>/css/style.css' : '<%= config.assets.src %>/stylus/style.styl',
 			'<%= config.assets.build %>/css/prioritize.css' : '<%= config.assets.src %>/stylus/prioritize.styl'
 		}
