cc138fb8236958b81faddb903f0f31ea3b59b910
Nicolas Huber <nicolasluishuber@gmail.com>
2019-03-01 09:59:36 -0300

    Ajustado para exibir os posts privados na área restrita

--- src/includes/Query/Post.php
+++ src/includes/Query/Post.php
@@ -30,7 +30,7 @@ class Post extends Base {
 	  * @param WP_Query $query A consulta que está sendo processada.
 	  */
 	public function hide_private( $query ) {
-		if ( ! is_admin() && ! is_preview() ) {
+		if ( ! is_admin() && ! is_preview() && ! is_page_template( 'page-templates/restricted-area.php' ) ) {
 			$query->set( 'post_status', 'publish' );
 		}
 	}
