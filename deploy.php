34f1afc31f6b5c4ce5901f3a2b06285962a42bef
Nicolas Huber <nicolasluishuber@gmail.com>
2019-03-12 10:35:11 -0300

    Revert "Atualizando .env e deploy.php para realizar deploy em produção"
    This reverts commit 6a33c076d779636f3f9c1a576f6eb19d74b22604.

--- deploy.php
+++ deploy.php
@@ -11,9 +11,9 @@ require 'app/recipe/grunt.php';
 
 host('186.237.59.129')
 	->port(2201)
-	->stage('production')
+	->stage('staging')
 	->user('elemarjr')
-	->set('deploy_path', '/home/elemarjr/production')
+	->set('deploy_path', '/home/elemarjr')
 	->set('http_user', 'www-data');
 
 set('repository', 'git@greatcode.aztecweb.net:aztecwebteam/elemarjr.git');
