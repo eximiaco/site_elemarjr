6a2aeb0fea0b55596a491bd98246508d0b71e378
Nicolas Huber <nicolasluishuber@gmail.com>
2019-04-15 09:37:36 -0300

    Fix title format to remove "private" and 'protected" from title

--- src/includes/Pages/Single.php
+++ src/includes/Pages/Single.php
@@ -19,6 +19,8 @@ class Single extends Base {
 	 */
 	public function init() {
 		add_filter( 'wp_link_pages_link', $this->callback( 'custom_post_page_link' ), 10, 2 );
+		add_filter( 'private_title_format', $this->callback( 'fix_title_format' ) );
+		add_filter( 'protected_title_format', $this->callback( 'fix_title_format' ) );
 	}
 
 	/**
@@ -30,5 +32,14 @@ class Single extends Base {
 	 */
 	public function custom_post_page_link( $link, $i ) {
 		return preg_replace( '/(<a.*href=".*")(.*)/', '$1 class="button"$2', $link );
+	}
+
+	/**
+	 * Fix title format.
+	 *
+	 * @return string
+	 */
+	public function fix_title_format() {
+		return '%s';
 	}
 }
