2dc5198ff153495dcb8dcbc92d4b87acfb870a13
Gabriel Rempel <gabriel@aztecweb.net>
2020-04-03 11:37:46 -0300

    Adicinoado scroll top ao tema

--- inc/src/aztlan/integration/class-ocean-wp.php
+++ inc/src/aztlan/integration/class-ocean-wp.php
@@ -17,6 +17,13 @@ class Ocean_Wp extends Base {
 	 * Add hooks and filters
 	 */
 	public function init() {
+		add_action( 'ocean_after_content_wrap', $this->callback( 'add_scroll_top' ) );
+	}
 
+	/**
+	 * Add scroll top after content wrap.
+	 */
+	public function add_scroll_top() {
+		get_template_part( 'template-parts/scroll-top' );
 	}
 }
